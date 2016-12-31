2ce3b37d4cc94cf5a10a01f4aeef68a3800d7a98
liuqi <xsdxluqi3class@gmail.com>
2016-12-30 23:12:13 -0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
     member do
       post :join
       post :quit
-      
+
     end
      resources :posts
    end
+
+   namespace :account do
+    resources :groups
+  end
 
    root 'groups#index'
 end
